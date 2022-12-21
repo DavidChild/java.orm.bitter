bd11ec07b380ad0ff364da1d050aa3da2f9ad38a
davidchild <844305358@qq.com>
2022-12-21 16:42:56 +0800

    refator(code): init datasource for druid datasoruce and origin datasource

--- bitter-core/src/main/java/io/github/davidchild/bitter/connection/DataSourceFactory.java
+++ bitter-core/src/main/java/io/github/davidchild/bitter/connection/DataSourceFactory.java
@@ -1,13 +1,20 @@
 package io.github.davidchild.bitter.connection;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import io.github.davidchild.bitter.exception.DataSourceException;
+import io.github.davidchild.bitter.tools.BitterLogUtil;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
 
 public class DataSourceFactory {
 
     private static DataSourceType dataSourceType;
 
     private static DruidDataSource druidDataSource;
+
+    private static DataSource dataSource;
 
     private static AbstractRoutingDataSource dynamicSource;
 
@@ -16,14 +23,38 @@ public class DataSourceFactory {
         if (dynamicSource != null)
             return;
         dynamicSource = dataSources;
+        dataSourceType = DataSourceType.AbstractRoutingDataSource;
 
     }
 
-    public static AbstractRoutingDataSource getDynamicSource() {
-        if (dynamicSource == null) {
-            // auto find the datasource of this project
+    public static void setDataSourceDruidDataSources(DruidDataSource dataSources) {
+        if (dynamicSource != null)
+            return;
+        druidDataSource = dataSources;
+        dataSourceType = DataSourceType.Druid;
 
+    }
+
+    public static void setDataSourceOriginDataSources(DataSource dataSources) {
+        if (dynamicSource != null)
+            return;
+        dataSource = dataSources;
+        dataSourceType = DataSourceType.OriginDataSource;
+
+    }
+
+    public static Connection getDataSourceConnection() {
+        try {
+            if (dynamicSource != null)
+                return dynamicSource.getConnection();
+            if (druidDataSource != null)
+                return dynamicSource.getConnection();
+            if (dataSource != null) return dataSource.getConnection();
+        } catch (Exception ex) {
+            BitterLogUtil.getInstance().error("bitter get the sql connection error.pls check your sql datasource setting and adjust that.");
+            throw new DataSourceException("bitter get the sql connection error,error detail msg is ------\r\n" + ex.getMessage());
         }
-        return dynamicSource;
+
+        throw new DataSourceException("bitter failed to automatically obtain the DRUID data source in your project or the data source implemented by AbstractRoutingDataSource");
     }
 }
---
+++ bitter-core/src/main/java/io/github/davidchild/bitter/exception/DataSourceException.java
@@ -0,0 +1,10 @@
+package io.github.davidchild.bitter.exception;
+
+public class DataSourceException extends RuntimeException {
+
+    public DataSourceException(String msg) {
+        super(msg);
+    }
+
+
+}
